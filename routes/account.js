e30533e6d3a23741f5154ae2685d9685eb7fceef
Tosh-Patil <77088510+Tosh-Patil@users.noreply.github.com>
2024-08-07 18:46:31 +1000

    Async SQL routes

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/login", function (req, res, next) {
+router.get("/login", async function (req, res) {
 
 	const username = req.body.username;
 	const password = req.body.password; 
-	var valid_login = sql`SELECT ID FROM Accounts WHERE Username = ${username} AND Password = ${password}`;
+	var valid_login = await sql`SELECT ID FROM Accounts WHERE Username = ${username} AND Password = ${password}`;
 	valid_login = valid_login[0];
 
 	if (!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password')) {
