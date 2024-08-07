34a04101b2a3bcdce631d5c8eb87a6940ce5af2d
Tosh-Patil <77088510+Tosh-Patil@users.noreply.github.com>
2024-08-07 20:06:42 +1000

    Response

--- routes/account.js
+++ routes/account.js
@@ -3,17 +3,18 @@ var router = express.Router();
 
 router.get("/login", async function (req, res) {
 
-	const username = req.body.username;
-	const password = req.body.password; 
-	var valid_login = await sql`SELECT ID FROM Accounts WHERE Username = ${username} AND Password = ${password}`;
-	valid_login = valid_login[0];
-
 	if (!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password')) {
 		res.status(403);
 		res.json({message: "No username or password specified"});
 	}
 
-	else if (valid_login == null || valid_login == "") {
+	const username = req.body.username;
+	const password = req.body.password; 
+	var valid_login = await sql`SELECT ID FROM Accounts WHERE Username = ${username} AND Password = ${password}`;
+	valid_login = valid_login[0];
+
+	
+	if (valid_login == null || valid_login == "") {
 		res.status(404);
 		res.json({message: "User not found"});
 	}
