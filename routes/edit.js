61193c3b42eef70302db0d398bde55b9f7bdc5e1
t-p7 <77088510+t-p7@users.noreply.github.com>
2024-09-27 10:52:45 +1000

    Delete function added

--- routes/edit.js
+++ routes/edit.js
@@ -1125,4 +1125,24 @@ router.post("/edit/furniture/:id", async function (req, res) {
     }
 });
 
+router.post("/delete/furniture/:id", async function (req, res) {
+	furniture_details = await sql`SELECT * FROM "Furniture" WHERE "Furniture"."FurnitureID" = ${sql.unsafe(req.params.id)}`;
+
+	await sql`DELETE FROM "Furniture" WHERE "Furniture"."FurnitureID" = ${sql.unsafe(req.params.id)}`;
+
+	details_check = await sql`SELECT * FROM "Furniture" WHERE "Furniture"."DetailsID" = ${furniture_details[0].DeatailsID}`
+
+	if (details_check === null || details_check === undefined || details_check = "") {
+		await sql`DELETE FROM "Details" WHERE "Details"."DetailsID"`;
+	}
+
+	await sql`DELETE FROM "UPIC" WHERE "UPIC"."UPIC" = ${furniture_details[0].UPIC}`;
+
+	await sql`DELETE FROM "Specification" WHERE "Specification"."FurnitureID" = ${req.params.id}`;
+
+	await sql`DELETE FROM "Delivery_&_Service_Region_Pricing" WHERE "Delivery_&_Service_Region_Pricing"."FurnitureID" = ${req.params.id}`;
+
+	res.status(200).json({message: `Deleted Furniture item: ${req.params.id}`});
+});
+
 module.exports = router;
